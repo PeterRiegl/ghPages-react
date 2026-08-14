86e149c626230c8d2df9b4bce715e17bcc837a17
PeterRiegl <peter.riegl@thi.de>
2026-08-14 13:58:14 +0200

    neues branch

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to Devops channel</h1>
         <p>
           Welcome to devOpsChannel. Please subscribe if you like my sharing!
         </p>
